699f58865f8418563eb8012c1419a0cf25de2706
tomograph <memo@di.ku.dk>
2020-02-13 16:31:49 +0100

    sperm phantom giving binary output

--- test/test_sperm_phantom.jl
+++ test/test_sperm_phantom.jl
@@ -3,6 +3,8 @@ using Logging
 using Plots
 using ProgressMeter
 using Dierckx
+using PolygonOps
+using StaticArrays
 
 #### These should be moved to another file along with vedranas method
 function segment_length(a::Array{Float64,1},b::Array{Float64,1})
@@ -36,6 +38,25 @@ function get_outline(centerline_points, radius_func)
     return (outline_xy, normal)
 end
 
+#Makes a matrix where the matrix entry is true iff the center of corresponding pixel is not outside the closed curve
+#curve is closed curve where first and last point should be the same.
+#xs and ys denote the center of each pixel column/rownorm
+function closed_curve_to_binary_mat(curve::Array{T1,2}, xs::Array{T2,1}, ys::Array{T2,1}) where {T1,T2 <: Number}
+    N = length(curve[:,1])
+    poly = map(i -> SVector{2,Float64}(curve[i,1],curve[i,2]), 1:N)
+    result = zeros(Int64, length(xs), length(ys))
+    i = 0
+    for x in xs
+        i += 1
+        j = 0#length(ys)+1
+        for y in ys
+            j +=1
+            result[j,i] = inpolygon(SVector(x,y), poly)
+        end
+    end
+    return result
+end
+
 cwd = @__DIR__
 path = normpath(joinpath(@__DIR__, "phantoms"))
 cd(path)
@@ -67,9 +88,14 @@ r(s) = 1.0
 anim = @animate for t=1:frames
     #Remove all zero rows (missing data points)
     non_zeros = filter(i ->  any(v-> v !== 0.0, time_sequence[i,:,t]) ,1:points)
-    plot(xlims=[-35,35], ylims =[-35, 35], aspect_ratio=:equal,framestyle=:none, legend=false)
+    plot( aspect_ratio=:equal,framestyle=:none, legend=false)
+    #determine outline from skeleton
     outline, normals = get_outline(reshape(time_sequence[non_zeros,:,t], (length(non_zeros),2)), r)
-    plot!(outline[:,1], outline[:,2,])
+    #close the curve
+    outline = cat(outline, outline[1,:]', dims=1)
+    #convert to binary image
+    mat = closed_curve_to_binary_mat(outline,collect(-38.0:0.2:38.0),collect(-38.0:0.2:38.0))
+    plot!(Gray.(mat))
     next!(p)
 end
 
