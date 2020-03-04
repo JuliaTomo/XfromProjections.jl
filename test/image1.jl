f2bceaed95d89a29e2aeab3d112b61b2b3c2098c
Mette Bjerg Lindhøj <memo@di.ku.dk>
2020-03-04 23:12:34 +0100

    plot mirror frames

--- test/image1.jl
+++ test/image1.jl
@@ -5,12 +5,7 @@ using ProgressMeter
 using XfromProjections.curve_utils
 using XfromProjections.snake_forward
 using XfromProjections
-# using Images
-# using Random
-# using ImageCore
-# using Distributions
-# using IterTools
-# using Dierckx
+using Printf
 using LinearAlgebra
 
 function translate_points(list_points::Array{T,2}, translation::AbstractArray{T,2}) where T<:AbstractFloat
@@ -54,104 +49,108 @@ map(t -> time_sequence[2:end,:,t] = data[(t-1)*points+1:(t-1)*points+points,:], 
 #Radius function
 r(s) = 1.0
 bins = collect(-38.0:0.125:38.0)
-
-frame_nr = 7
-nangles = 1
-angles = [π/2]
-
-
-#Remove all zero rows (missing data points)
-non_zeros = filter(j ->  any(v-> v !== 0.0, time_sequence[j,:,frame_nr]) ,1:points)
-prepend!(non_zeros,1)
-
-#determine outline from skeleton
-outline, normals = get_outline(reshape(time_sequence[non_zeros,:,frame_nr], (length(non_zeros),2)), r)
-sinogram = parallel_forward(outline, angles, bins)
-ground_truth = reshape(time_sequence[non_zeros,:,frame_nr], (length(non_zeros),2))
-
-num_points = 30
-
-template_y = range(0.0, -35.0, length=num_points)
-template_x = zeros(num_points)
-template = cat(template_x, template_y, dims = 2)
-
-plt = plot(template[:,1], template[:,2], aspect_ratio=:equal, label="template")
-plot!(outline[:,1], outline[:,2], label = "target")
-
-x_line = cat(bins,zeros(length(bins)),dims=2)
-y_vals = cat(bins,sinogram,dims=2)
-x_line = rotate_points(translate_points(x_line, [0.0 20.0]), angles[1])
-y_vals = rotate_points(translate_points(y_vals, [0.0 20.0]), angles[1])
+cwd = @__DIR__
+p = Progress(10, 1)
+for frame_nr = 1:10
+#frame_nr = 2
+    nangles = 1
+    angles = [π/2]
 
 
+    #Remove all zero rows (missing data points)
+    non_zeros = filter(j ->  any(v-> v !== 0.0, time_sequence[j,:,frame_nr]) ,1:points)
+    prepend!(non_zeros,1)
 
-plot!(y_vals[10:end-250,1], y_vals[10:end-250,2], label="sinogram")
-n = 1000
-p = Progress(n, 1)
-recon1 = deepcopy(template)
-recon2 = deepcopy(template)
-function find_largest_discrepancy(sino1,sino2)
-    diff = abs.(sino1-sino2)
-    i = argmax(diff)
-    bin_max = bins[i]
-    return bin_max
-end
+    #determine outline from skeleton
+    outline, normals = get_outline(reshape(time_sequence[non_zeros,:,frame_nr], (length(non_zeros),2)), r)
+    sinogram = parallel_forward(outline, angles, bins)
+    ground_truth = reshape(time_sequence[non_zeros,:,frame_nr], (length(non_zeros),2))
 
-w_u = ones(num_points)
-w_u[1] = 0.0
-w_l = ones(num_points)
-w_l[1] = 0.0
-residual = 1000
-anim1 = @animate for i=1:n
-    plot(deepcopy(plt))
-    global recon1 = recon2d_tail(recon1,r,angles,bins,sinogram,1, 0.0, 0.1, 1, w_u, zeros(num_points))
+    num_points = 30
 
-    global recon2 = recon2d_tail(recon2,r,angles,bins,sinogram,1, 0.0, 0.1, 1, zeros(num_points), w_l)
+    template_y = range(0.0, -35.0, length=num_points)
+    template_x = zeros(num_points)
+    template = cat(template_x, template_y, dims = 2)
+
+    plt = plot(template[:,1], template[:,2], aspect_ratio=:equal, label="template")
+    plot!(outline[:,1], outline[:,2], label = "target")
+
+    x_line = cat(bins,zeros(length(bins)),dims=2)
+    y_vals = cat(bins,sinogram,dims=2)
+    x_line = rotate_points(translate_points(x_line, [0.0 20.0]), angles[1])
+    y_vals = rotate_points(translate_points(y_vals, [0.0 20.0]), angles[1])
+
+    plot!(y_vals[10:end-250,1], y_vals[10:end-250,2], label="sinogram")
+    n = 5000
+    recon1 = deepcopy(template)
+    recon2 = deepcopy(template)
+    function find_largest_discrepancy(sino1,sino2)
+        diff = abs.(sino1-sino2)
+        i = argmax(diff)
+        bin_max = bins[i]
+        return bin_max
+    end
+
+    w_u = ones(num_points)
+    w_u[1] = 0.0
+    w_l = ones(num_points)
+    w_l[1] = 0.0
+    residual = 1000
+    for i=1:n
+        plot(deepcopy(plt))
+        recon1 = recon2d_tail(recon1,r,angles,bins,sinogram,1, 0.0, 0.1, 1, w_u, zeros(num_points))
+        recon2 = recon2d_tail(recon2,r,angles,bins,sinogram,1, 0.0, 0.1, 1, zeros(num_points), w_l)
+
+        outline1, normals = get_outline(recon1, r)
+        s1 = parallel_forward(outline1, angles, bins)
+        outline2, normals = get_outline(recon2, r)
+        s2 = parallel_forward(outline2, angles, bins)
+        residual_mirror = norm(s2-s1)
+        residual_1 = norm(sinogram-s1)
+        residual_2 = norm(sinogram-s2)
+
+        #if residual_mirror < 10e-2 && residual_1 > 1 && residual_2 > 1 && residual_1 < 6 && residual_2 < 6
+        b1 = find_largest_discrepancy(sinogram, s1)
+        b2 = find_largest_discrepancy(sinogram, s2)
+        #@info b1,b2
+        angle = angles[1]
+        projection = [cos(angle) sin(angle)]'
+        vertex_coordinates1 = (recon1*projection)[:,1]
+        vertex_coordinates2 = (recon2*projection)[:,1]
+
+        v1 = findfirst(v -> v < b1, vertex_coordinates1)
+        v2 = findfirst(v -> v < b2, vertex_coordinates2)
+        if !isnothing(v1) && !isnothing(v2) && (v1 > 1 && v1 < num_points && v2 > 1 && v2 < num_points)
+            needed_translation1 = (recon1[v1,:]-recon2[v1+1,:].+0.1)'
+            needed_translation2 = (recon2[v2,:]-recon1[v2+1,:].+0.1)'
+            temp = deepcopy(recon1)
+            recon1 = cat(recon1[1:v1,:], translate_points(recon2[(v1+1):end,:],needed_translation1), dims=1)
+            recon2 = cat(recon2[1:v2,:], translate_points(temp[(v2+1):end,:],needed_translation2), dims=1)
+        end
+
+    end
+
     plot!(recon1[:,1],recon1[:,2], label="upper")
     plot!(recon2[:,1],recon2[:,2], label="lower")
+
     outline1, normals = get_outline(recon1, r)
     s1 = parallel_forward(outline1, angles, bins)
     outline2, normals = get_outline(recon2, r)
     s2 = parallel_forward(outline2, angles, bins)
-    residual_mirror = norm(s2-s1)
-    residual_1 = norm(sinogram-s1)
-    residual_2 = norm(sinogram-s2)
 
-    if residual_mirror < 10e-6 && residual_1 > 1 && residual_2 > 1 && residual_1 < 6 && residual_2 < 6
-        b1 = find_largest_discrepancy(sinogram, s1)
-        b2 = find_largest_discrepancy(sinogram, s2)
-        if norm(b1-b2) < 10e-6
-            angle = angles[1]
-            projection = [cos(angle) sin(angle)]'
-            vertex_coordinates1 = (recon1*projection)[:,1]
-            vertex_coordinates2 = (recon2*projection)[:,1]
-
-            v1 = findfirst(v -> v < b1, vertex_coordinates1)
-
-            v2 = findfirst(v -> v < b2, vertex_coordinates2)
-            if v1 > 1 && v1 < num_points && v2 > 1 && v2 < num_points
-                needed_translation1 = (recon1[v1,:]-recon2[v1+1,:].+0.1)'
-                needed_translation2 = (recon2[v2,:]-recon1[v2+1,:].+0.1)'
-                temp = deepcopy(recon1)
-                global recon1 = cat(recon1[1:v1,:], translate_points(recon2[(v1+1):end,:],needed_translation1), dims=1)
-                global recon2 = cat(recon2[1:v2,:], translate_points(temp[(v2+1):end,:],needed_translation2), dims=1)
-            end
-        end
-    end
-    #global residual = norm(sinogram-s1)
     y_vals1 = cat(bins,s1,dims=2)
     y_vals1 = rotate_points(translate_points(y_vals1, [0.0 20.0]), angles[1])
     plot!(y_vals1[10:end-250,1], y_vals1[10:end-250,2], label="upper sinogram")
     y_vals2 = cat(bins,s2,dims=2)
     y_vals2 = rotate_points(translate_points(y_vals2, [0.0 20.0]), angles[1])
     plot!(y_vals2[10:end-250,1], y_vals2[10:end-250,2], label="lower sinogram")
+
+    path = normpath(joinpath(@__DIR__, "results"))
+    cd(path)
+    savefig(@sprintf "frame_%d" frame_nr)#gif(anim1, "evolution1.gif", fps = 10)
     next!(p)
 end
 
-cwd = @__DIR__
-path = normpath(joinpath(@__DIR__, "results"))
-cd(path)
-gif(anim1, "evolution1.gif", fps = 10)
 cd(cwd)
 
 plot!()
